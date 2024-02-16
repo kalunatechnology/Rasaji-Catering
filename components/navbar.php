7a4aba1149a6adfdc9dbb559b9257870e09818ed
ilyasskurnia <ilyaskurnia07@gmail.com>
2024-02-16 15:41:24 +0700

    update tampilan dan penyesuaian

---
+++ components/navbar.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
+    <a href="" class="navbar-brand p-0">
+        <img src="img/rasajilogo.png" alt="Logo" style="width: 100px;">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="fa fa-bars"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto py-0 pe-4">
+            <a href="index.php" class="nav-item nav-link">Home</a>
+            <a href="about.php" class="nav-item nav-link">About</a>
+            <a href="service.php" class="nav-item nav-link">Service</a>
+            <a href="menu.php" class="nav-item nav-link">Menu</a>
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                <div class="dropdown-menu m-0">
+                    <a href="booking.php" class="dropdown-item">Booking</a>
+                    <a href="team.php" class="dropdown-item">Our Team</a>
+                    <a href="testimonial.php" class="dropdown-item">Testimonial</a>
+                </div>
+            </div>
+            <a href="contact.php" class="nav-item nav-link">Contact</a>
+        </div>
+        <a href="" class="btn btn-primary py-2 px-4">Book A Table</a>
+    </div>
+</nav>
